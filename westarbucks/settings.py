6dd44a1d9b050b0d695697b596c6fffd430105a4
phin09 <sy6kang@gmail.com>
2021-01-28 19:22:08 +0900

    reset products app migrations

--- westarbucks/settings.py
+++ westarbucks/settings.py
@@ -118,3 +118,31 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#REMOVE_APPEND_SLASH_WARNING
+APPEND_SLASH = False
+
+##CORS
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    #만일 허용해야 될 추가적인 헤더키가 있다면(사용자정의 키) 여기에 추가
+)
